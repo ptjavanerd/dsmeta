63ca817f955c4cd7557d744b883b2dcd7705f6a8
Carlos <carlos@ifuturo.net>
2022-07-18 18:25:27 +0100

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
 return(
+  <>
   <h1>Olá Mundo!</h1>
+  <NotificationButton />
+  <NotificationButton />
+  <NotificationButton />
+  <NotificationButton />
+  <NotificationButton />
+  
+</>
 )
 }
 
